2bbaa2d07cce011c6ca3c9de5e4fbde0f20f9139
Mark <wushu4@yahoo.com.vn>
2021-09-23 18:38:22 -0400

    Added movement to character

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,6 +9,8 @@ public class PlayerBehaviour : MonoBehaviour
 
     [SerializeField]
     private Rigidbody2D rb = null;
+
+
     // Start is called before the first frame update
     void Start()
     {
