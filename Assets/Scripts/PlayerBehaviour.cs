a873ac8d94548ea8ad929524ac5e78ec286b88b0
Mark <wushu4@yahoo.com.vn>
2021-09-23 18:16:10 -0400

    Added project files

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+
+    [SerializeField]
+    private Rigidbody2D rb = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, Input.GetAxisRaw("Vertical") * moveSpeed);
+    }
+}
